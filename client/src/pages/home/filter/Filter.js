d7403915e14e89afa583cbec9e18e58ab3725300
khandakerJasim <md.joshimuddin975@gmail.com>
2024-06-04 13:18:47 +0600

    filter and find data by gender

--- client/src/pages/home/filter/Filter.js
+++ client/src/pages/home/filter/Filter.js
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from "react";
+import { DataState } from "../../../component/context/Contextprovider";
 //import { getfunc } from "../../service/Apis";
 
 export default function Filter() {
   const [gender, setgender] = useState("All");
 
   useEffect(() => {}, [gender]);
+  const {
+    productdispatch,
+    productstate: { bygender },
+  } = DataState();
 
   return (
     <div className="">
       <div className="font-bold">
         <h1 className="text-xl text-center ">filter by gender</h1>
 
-        <div className="flex gap-4 pt-3 items-center text-center justify-center ">
+        {/* <div className="flex gap-4 pt-3 items-center text-center justify-center ">
           <div>
             <input
               type="radio"
@@ -19,6 +24,7 @@ export default function Filter() {
               value={"All"}
               onChange={(e) => setgender(e.target.value)}
               defaultChecked
+            
             />
             <label htmlFor="">All</label>
           </div>
@@ -40,7 +46,35 @@ export default function Filter() {
             />
             <label htmlFor="">Female</label>
           </div>
-        </div>
+        </div> */}
+
+        <ul className="list-none  text-center">
+          <button
+            className=" hover:text-red-400 inline-block focus:outline-none focus:border-b border-red-400 focus:outline-none focus:border-b border-red-400"
+            onClick={() =>
+              productdispatch({
+                type: "filter_by_gender",
+                payload: "male",
+              })
+            }
+            checked={bygender}
+          >
+            Men
+          </button>
+          <br />
+          <button
+            className="cursor-pointer hover:text-red-400 inline-block focus:outline-none focus:border-b border-red-400 "
+            onClick={() =>
+              productdispatch({
+                type: "filter_by_gender",
+                payload: "female",
+              })
+            }
+            checked={bygender}
+          >
+            Women
+          </button>
+        </ul>
       </div>
     </div>
   );
