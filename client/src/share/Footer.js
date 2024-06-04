55fe31f9ad65dc6ad535714430dbfe53dbaa6620
khandakerJasim <md.joshimuddin975@gmail.com>
2024-06-04 09:50:45 +0600

    server side update get data from json file and find data by id

--- client/src/share/Footer.js
+++ client/src/share/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="bg-gray-200 dark:bg-black dark:text-white w-full h-auto">
+    <div className="bg-gray-200 dark:bg-black dark:text-white w-full h-full py-4">
       <h1 className="text-center text-2xl font-bold">it is Footer component</h1>
     </div>
   );
--- server/app.js
+++ server/app.js
@@ -2,19 +2,19 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const Bodyparser = require("body-parser");
-const mongoose = require("mongoose");
-const Errorhandler = require("./middleware/Error");
-const router = require("./routes/Router");
-const userrouter = require("./routes/usRouter");
+// const mongoose = require("mongoose");
+// const Errorhandler = require("./middleware/Error");
+// const router = require("./routes/Router");
+// const userrouter = require("./routes/usRouter");
 
 const app = express();
 
 //mongoose connnect
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/searchpagination")
-  .then(() => console.log("mongoose connect successfully"))
-  .catch((error) => console.log("error", error));
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/searchpagination")
+//   .then(() => console.log("mongoose connect successfully"))
+//   .catch((error) => console.log("error", error));
 
 //cors setup
 app.use(cors());
@@ -25,18 +25,30 @@ app.use(Bodyparser.json());
 
 //dot env config
 dotenv.config();
+const data = require("./Data/data.json");
+
+app.get("/data", (req, res) => {
+  res.send(data);
+});
+
+//find data by id
+app.get("/data/:id", (req, res) => {
+  const id = req.params.id;
+  const result = data.find((n) => n.id === id);
+  res.send(result);
+});
 
 //routeruse
 
-app.use(router);
+// app.use(router);
 
-//usrouteruse
-app.use(userrouter);
+// //usrouteruse
+// app.use(userrouter);
 
-//static folder use
-app.use("/uploads", express.static("./uploads"));
+// //static folder use
+// app.use("/uploads", express.static("./uploads"));
 
-//errorhandler
-app.use(Errorhandler);
+// //errorhandler
+// app.use(Errorhandler);
 
 module.exports = app;
